3cd289c6a85330b1f145291893aafa29c93160cc
Doaa Kadhim <doaa.abdulwahed88@gmail.com>
2023-12-24 21:28:57 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const element = document.getElementById("main");
+element.remove(); 
+
+var newHeader = document.createElement('h1');
+newHeader.textContent = 'New Header Content';
+newHeader.id='victory';
+newHeader.textContent += ' is the champion';
